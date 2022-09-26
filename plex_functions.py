84511e80b8ca6f3d66f41accc25f5e676667725c
Matt Bittinger <mattsbitt@gmail.com>
2022-09-26 11:45:24 -0500

    vscode

--- plex_functions.py
+++ plex_functions.py
@@ -1,5 +1,6 @@
 from plexapi.server import PlexServer
 
+
 def plex_update_library(plex, library):
 
     harrymack = plex.library.section(library)
@@ -9,5 +10,41 @@ def connect_to_server():
     baseurl = 'http://192.168.0.202:32400' #the local address of your server
     token = 'ao7MsVLaXxRWLtCoyXWm'
     return PlexServer(baseurl, token)
+
+def get_playlists(plex):
+    for playlist in plex.playlists():
+        print(playlist.title)
+
+def search_tracks(plex, library, track_title):
+    lib = plex.library.section(library)
+    t = lib.searchTracks(title=track_title)
+    return t
+
+def add_mood(plex, library, track_title, mood):
+    t = search_tracks(plex, library, track_title)
+    if len(t) < 1:
+        print(f"Track {track_title} not found.  {t}")
+        return False
+    elif len(t) > 1:
+        print(f"Too many matches found for {track_title}.  {t}")
+        return False
+    else:
+        t[0].addMood(mood)
+        return True
+
+
 if __name__ == "__main__":
-    pass
+    plex = connect_to_server()
+    get_playlists(plex)
+    lib = plex.library.section("Harry Mack")
+    #for track in lib.all():
+        #print(album.tracks()[0].media[0].parts[0].file)
+    #    print(track)
+
+    #availableFields = [f.key.split('.')[-1] for f in lib.listFields()]
+    #print("Available fields:", availableFields)
+    #print(len(lib.searchTracks()))
+    #print(lib.searchTracks(title="BTB11.1 Intro"))
+    search_tracks(plex, "Harry Mack", "BTB11.1 Intro")
+        
+    
